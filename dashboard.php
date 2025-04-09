4554617182d232a586e77b391ea98f5578fee3a3
Yllza <kryeziuyllza00@gmail.com>
2025-04-10 01:37:49 +0200

    venue dashboard

--- dashboard.php
+++ dashboard.php
@@ -51,64 +51,6 @@ $recent_reservations = mysqli_query($conn, "
             padding: 0;
         }
 
-        .navbar {
-            top: 0;
-            left: 0;
-            right: 0;
-            width: 100%;
-            box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
-            border: 0;
-            background-color: #f7d8db;
-            z-index: 100;
-            padding: 20px 0;
-        }
-
-        .navbar-brand {
-            font-size: 30px;
-            font-family: 'Playfair Display', serif;
-            font-weight: bold;
-            color: #7a4d56; /* Soft wedding pink color */
-            margin-right: 20px;
-        }
-
-        .navbar-brand img {
-            width: 190px; /* Placeholder for logo */
-            height: auto;
-            object-fit: contain;
-        }
-
-        .nav-pills .nav-item .nav-link {
-            box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
-            background-color: #e3a2b3; /* Soft wedding pink */
-            border-radius: 30px;
-            color: white;
-            padding: 10px 20px;
-            font-family: 'Playfair Display', serif;
-        }
-
-        .nav-pills .nav-item .nav-link:hover {
-            background-color: #d36b7f;
-            color: white;
-        }
-
-        .nav-pills .nav-item .nav-link.active {
-            background-color: #7a4d56; /* Elegant color for active links */
-            border-radius: 30px;
-        }
-
-        .navbar-toggler {
-            border-color: #7a4d56;
-        }
-
-        .tab-content {
-            height: 100%;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
         .container {
             padding: 30px;
             max-width: 1100px;
@@ -154,53 +96,11 @@ $recent_reservations = mysqli_query($conn, "
             border-bottom: 1px solid #ddd;
         }
 
-        nav {
-            text-align: center;
-            margin-top: 30px;
-        }
-
-        nav a {
-            margin: 0 15px;
-            color: #B76E79;
-            text-decoration: none;
-            font-weight: bold;
-        }
-
-        nav a:hover {
-            text-decoration: underline;
-        }
+       
     </style>
 </head>
 <body>
-<div class="header navbar navbar-expand-lg navbar-light">
-<div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="images/logo-tr.png" alt="Logo" /> 
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-lg-end" id="navbarSupportedContent">
-                <ul class="nav nav-pills align-right flex-column flex-lg-row p-2" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" href="dashboard.php">Home</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="manage_venues.php">Venues</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="manage_reservations.html">Reservations</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="manage_admins.php">Admins</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="logout.php">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+<?php include('header_admin.php'); ?>
 
     <div class="container">
         <div class="summary-cards">
