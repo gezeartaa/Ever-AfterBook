8909836953e0382448069bf9c266a0af3a618b08
Yllza <kryeziuyllza00@gmail.com>
2025-05-21 01:39:21 +0200

    db change

--- db_connection.php
+++ db_connection.php
@@ -1,29 +1,29 @@
 <?php
-// $servername = "sql7.freesqldatabase.com";
-// $username = "sql7773842";  //username (krejt qeto ti jep kru e qel databazen ti gjeneron vete)
-// $password = "A96BRl1CDr";       // passwordi
-// $dbname = "sql7773842"; //emri i databazes   
+$servername = "sql7.freesqldatabase.com";
+$username = "sql7780151";  //username (krejt qeto ti jep kru e qel databazen ti gjeneron vete)
+$password = "Ubl1tuhM8h";       // passwordi
+$dbname = "sql7780151"; //emri i databazes   
 
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-// echo "Connected successfully";
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
 
 <?php
-$servername = "localhost"; // Use "localhost" for local MySQL server
-$username = "root";        // XAMPP default MySQL username
-$password = "";            // Default MySQL password is empty ("" in XAMPP)
-$dbname = "everafterbook"; // Your database name (same as in phpMyAdmin)
+// $servername = "localhost"; // Use "localhost" for local MySQL server
+// $username = "root";        // XAMPP default MySQL username
+// $password = "";            // Default MySQL password is empty ("" in XAMPP)
+// $dbname = "everafterbook"; // Your database name (same as in phpMyAdmin)
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 ?>
